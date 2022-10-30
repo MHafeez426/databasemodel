4f882e7c4e9208e4b3b16e8ee5aca439eb17628c
MHafeez426 <mohamedhafeez111@gmail.com>
2022-10-30 23:39:08 +0530

    commit

--- lib/hive/Hiveassignmt/TODO hive.dart
+++ lib/hive/Hiveassignmt/TODO hive.dart
@@ -6,7 +6,7 @@ import 'package:hive_flutter/hive_flutter.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
-  await Hive.openBox('Todo_note');
+  await Hive.openBox('Todo_notes');
   runApp(MaterialApp(
     home: HiveTodo(),
     debugShowCheckedModeBanner: false,
